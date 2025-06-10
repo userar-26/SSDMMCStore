db04006eb1b655778ae4d040652b2332ea0f9466
userar-26 <arhipenko.roman26@gmail.com>
2025-06-10 15:46:20 +0300

    Изменил конфигурацию

---
+++ src/key_value_store/kv_store_internal.h
@@ -0,0 +1,8 @@
+#ifndef SSDMMCSTORE_KV_STORE_INTERNAL_H
+#define SSDMMCSTORE_KV_STORE_INTERNAL_H
+
+
+#include "kv_store.h"
+
+
+#endif
---
+++ src/ssdmmc_sim/ssdmmc_sim_internal.h
@@ -0,0 +1,6 @@
+#ifndef SSDMMCSTORE_SSDMMC_SIM_INTERNAL_H
+#define SSDMMCSTORE_SSDMMC_SIM_INTERNAL_H
+
+#include "ssdmmc_sim.h"
+
+#endif
